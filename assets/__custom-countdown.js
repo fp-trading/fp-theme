a0c7e21eb27f863858fec8d849abcf5ea4685320
Jonas Abrahams <jonas.abrahams@farben-profi.de>
2023-11-20 11:06:25 +0100

    countdown

--- assets/__custom-countdown.js
+++ assets/__custom-countdown.js
@@ -4,15 +4,21 @@ const countdowns = document.querySelectorAll('span.announcement-bar__countdown')
 
 countdowns.forEach(countdown => countdown.target = new Date(countdown.dataset['target']).getTime())
 
-setInterval(() => {
-    countdowns.forEach(countdown => {
-        const distance = countdown.target - new Date().getTime();
+if (countdowns.length > 0) {
+    setInterval(() => {
+        countdowns.forEach(countdown => {
+            const distance = countdown.target - new Date().getTime();
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-        const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-        const seconds = Math.floor(distance % (1000 * 60) / (1000));
+            if (distance > 0) {
+                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                const hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+                const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+                const seconds = Math.floor(distance % (1000 * 60) / (1000));
 
-        countdown.innerHTML = `${days}t ${hours}std ${minutes}min ${seconds}s`
-    })
-}, 1000)
+                countdown.innerHTML = `${days}t ${hours}std ${minutes}min ${seconds}s`
+            } else {
+                countdown.innerHTML = "";
+            }
+        })
+    }, 1000)
+}
